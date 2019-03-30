2531b26bc36889956b5a9251749dc9149bba58a2
Elle Valentine <elle.valentine@hotmail.com>
2019-03-30 16:37:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,54 @@
-def dictionary 
-  
-  words-to-be{
-      :hello => "hi",
-      :to => 2, 
-      :two => 2,
-      :too => 2, 
-      :for => 4, 
-      :four => 4, 
-      :be => "b", 
-      :you => "u", 
-      :at => @, 
-      :and => "&"
+var abbr_dictionary = {
+	"hello" : "hi",
+	"to" : "2",
+	"two" : "2",
+	"too" : "2",
+	"be" : "b",
+	"you" : "u",
+	"at" : "@",
+	"and" : "&",
+	"for" : "4",
+	"For" : "4"
+};
+
+var tweetShortener = {
+ wordSubstituter: function(tweet){
+   var subbed_tweet = [];
+   tweet.split(" ").forEach(function(word){
+   	if (abbr_dictionary[word] === undefined){
+   		subbed_tweet.push(word);
+   	} else {
+   		subbed_tweet.push(abbr_dictionary[word]);
+   	}
+   });
+   return subbed_tweet.join(" ");
+ },
+ bulkShortener: function(tweets){
+ 	var wordSubstituter = this.wordSubstituter;
+ 	var short_tweet = [];
+
+ 	tweets.forEach(function(tweet){
+ 		short_tweet.push(wordSubstituter(tweet));
+ 	});
+ 	return short_tweet;
+ },
+ selectiveShortener: function(tweet){
+    var wordSubstituter = this.wordSubstituter;
+
+    if (tweet.length > 140){
+      return wordSubstituter(tweet);
+    } else {
+      return tweet;
     }
-end
+  },
+
+  shortenedTruncator: function(tweet){
+    var wordSubstituter = this.wordSubstituter;
+
+    if (tweet.length > 140){
+      return wordSubstituter(tweet).slice (0,137)+"...";
+    } else {
+      return tweet;
+    }
+  }
+};
