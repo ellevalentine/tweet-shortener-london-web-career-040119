18ba71da71983d1bc250706dd1f6147c4bfcaf17
Elle Valentine <elle.valentine@hotmail.com>
2019-03-30 16:34:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,7 @@
 def dictionary 
   
-  words-to-be{:hello => "hi", :to => 2, :two => 2, :too => 2, :for => 4, :four => 4, :be => "b", :you => "u", :at => @, :and => &
-    
-    
+  words-to-be{:hello => "hi", :to => 2, :two => 2, :too => 2, :for => 4, :four => 4, :be => "b", :you => "u", :at => @, :and => "&"
+  
   }
   
 end
